ddc296af9235b063845c81487deb45da7544657d
Jeff Huang <jeffshuang2@gmail.com>
2021-02-09 17:07:18 -0800

    Created error handling messages for geocoding request.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ request({ url: url, json: true }, (error, response) => {
         console.log("Unable to connect to weather service.") 
     }
     else if (response.body.error) {
-        console.log("Unable to find location")
+        console.log("Unable to find location.")
     }
     else {
         console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. There is a " + current.precip + "% chance of rain.")
@@ -22,8 +22,16 @@ const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Ange
 
 request({ url: geocodeURL, json: true}, (error, response) => {
 
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
+    if (error) {
+        console.log("Unable to connect to location services.")
+    }
+    else if (response.body.features.length === 0) {
+        console.log("Unable to find location requested. Please try again.")
+    }
+    else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 
-    console.log(latitude, longitude)
 })
