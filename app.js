e1a4456f136d5cb18dd1044cb987789ac6e464e7
Jeff Huang <jeffshuang2@gmail.com>
2021-02-10 16:48:03 -0800

    Reworked geocode functionality to be reusable throughout the app. Created separate file for it to be imported when needed.

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
 const request = require("request")
+const geocode = require("./utils/geocode")
 
-const url = "http://api.weatherstack.com/current?access_key=1b7bdcd4700f7f5c4802086411f80801&query=37.8267,-122.4233&units=f"
+// const url = "http://api.weatherstack.com/current?access_key=1b7bdcd4700f7f5c4802086411f80801&query=37.8267,-122.4233&units=f"
 
-request({ url: url, json: true }, (error, response) => {
-    const current = response.body.current
+// request({ url: url, json: true }, (error, response) => {
+//     const current = response.body.current
 
-    if (error) {
-        console.log("Unable to connect to weather service.") 
-    }
-    else if (response.body.error) {
-        console.log("Unable to find location.")
-    }
-    else {
-        console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. There is a " + current.precip + "% chance of rain.")
-    }
-})
+//     if (error) {
+//         console.log("Unable to connect to weather service.") 
+//     }
+//     else if (response.body.error) {
+//         console.log("Unable to find location.")
+//     }
+//     else {
+//         console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. There is a " + current.precip + "% chance of rain.")
+//     }
+// })
 
 
-
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiamh1YW5nMiIsImEiOiJja2tya2FzMWIwZGR0MnBsam5zejhmNzd1In0.c46vnzArKwxnChHgH7-wnQ&limit=1"
-
-request({ url: geocodeURL, json: true}, (error, response) => {
-
-    if (error) {
-        console.log("Unable to connect to location services.")
-    }
-    else if (response.body.features.length === 0) {
-        console.log("Unable to find location requested. Please try again.")
-    }
-    else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
-
+geocode("Hawaii", (error, data) => {
+    console.log("Error", error)
+    console.log("Data", data)
 })
