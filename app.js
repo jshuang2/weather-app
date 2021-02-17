8a87f438baf27af8d2baf25f33e1f750af9e4417
Jeff Huang <jeffshuang2@gmail.com>
2021-02-16 17:17:16 -0800

    Further reworked the geocode call to accept user input from the command line for a location to search. New code also catches errors if the user does not provide a location.

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
+const address = process.argv[2]
 
-geocode("Boston", (error, data) => {
-    if (error) {
-        return console.log("Error", error)
-    }
-
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+    console.log("Please provide an address.")
+}
+else {
+    geocode(address, (error, data) => {
         if (error) {
-            return console.log(error)
+            return console.log("Error", error)
         }
-        
-        console.log(data.location)
-        console.log(forecastData)
-        
-      })
-})
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+            console.log(data.location)
+            console.log(forecastData)
+            
+          })
+    })
+}
+
+
 
 
