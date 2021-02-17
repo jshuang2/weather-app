007915c4f77b46d8f5b4e7b1e8960d641bbe0725
Jeff Huang <jeffshuang2@gmail.com>
2021-02-16 16:49:30 -0800

    Reworked geocode and forecast calls to be cleaner. Also catching errors if they happen.

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const forecast = require("./utils/forecast")
 
 
 geocode("Boston", (error, data) => {
-    console.log("Error", error)
-    console.log("Data", data)
+    if (error) {
+        return console.log("Error", error)
+    }
 
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        
+        console.log(data.location)
+        console.log(forecastData)
+        
       })
 })
 
