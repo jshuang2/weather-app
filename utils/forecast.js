1e420cf260071f6c1654370ac242b65b833e38b4
Jeff Huang <jeffshuang2@gmail.com>
2021-02-18 14:17:07 -0800

    Simplified the code in all files via destructuring.

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,14 +4,14 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = "http://api.weatherstack.com/current?access_key=1b7bdcd4700f7f5c4802086411f80801&query=" + latitude + "," + longitude +  "&units=f"
 
-    request({url: url, json: true}, (error, response) => {
+    request({url: url, json: true}, (error, { body }) => {
 
-        const current = response.body.current
+        const { current } = body
 
         if (error) {
             callback("Unable to connect to weather service.", undefined)
         }
-        else if (response.body.error) {
+        else if (body.error) {
             console.log(callback)
             callback("Unable to find location.", undefined)
         }
